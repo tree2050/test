30731b41b44214e302a358e9410ddbdb2dc23a3d
zmy <ln@DESKTOP-PMIS49U>
2020-01-09 17:21:17 +0800

    practice

---
+++ src/test1/sprider.py
@@ -0,0 +1,12 @@
+print("hello World!")
+
+# Function definition is here
+def printme( str ):
+    "打印任何传入的字符串"
+    print (str)
+    return
+   
+ 
+# Now you can call printme function
+printme("我要调用用户自定义函数!");
+printme("再次调用同一函数");
